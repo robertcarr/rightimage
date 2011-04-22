2d5f48ccd0c76675b74fae73687ea0791fed11e0
Robert Carr <robert@rightscale.com>
2011-04-22 06:55:00 -0700

    Changed variables to [:node] attributes

--- cookbooks/rightimage/recipes/helper_create_image_and_rsync.rb
+++ cookbooks/rightimage/recipes/helper_create_image_and_rsync.rb
@@ -7,39 +7,39 @@ bash "Create disk image and copy filesystem contents" do
     set -e 
     set -x
 
-    DISK_SIZE_GB=#{image_size_gb}
+    DISK_SIZE_GB=#{node.rightimage.image_size_gb}
     BYTES_PER_MB=1024
     DISK_SIZE_MB=$(($DISK_SIZE_GB * $BYTES_PER_MB))
 
     source_image="#{node.rightimage.mount_dir}" 
-    target_raw_path="#{target_raw_path}"
-    target_mnt="#{target_mnt}"
+    target_raw_path="#{node.rightimage.target_raw_path}"
+    target_mnt="#{node.rightimage.target_mnt}"
 
-    umount -lf #{source_image}/proc || true 
-    umount -lf #{target_mnt}/proc || true 
-    umount -lf #{target_mnt} || true
+    umount -lf #{node.rightimage.source_image}/proc || true 
+    umount -lf #{node.rightimage.target_mnt}/proc || true 
+    umount -lf #{node.rightimage.target_mnt} || true
     rm -rf $target_raw_path $target_mnt
 
     dd if=/dev/zero of=$target_raw_path bs=1M count=$DISK_SIZE_MB    
 
-    umount -lf #{loop_map} || true
-    kpartx -d  #{loop_dev} || true
-    losetup -d #{loop_dev} || true
+    umount -lf #{node.rightimage.loop_map} || true
+    kpartx -d  #{node.rightimage.loop_dev} || true
+    losetup -d #{node.rightimage.loop_dev} || true
 
-    losetup #{loop_dev} $target_raw_path
+    losetup #{node.rightimage.loop_dev} $target_raw_path
 
-    sfdisk #{loop_dev} << EOF
+    sfdisk #{node.rightimage.loop_dev} << EOF
 0,1304,L
 EOF
    
-    kpartx -a #{loop_dev}
-    mke2fs -F -j #{loop_map}
+    kpartx -a #{node.rightimage.loop_dev}
+    mke2fs -F -j #{node.rightimage.loop_map}
     
     # setup uuid for our root partition
-    tune2fs -U #{node[:rightimage][:root_mount][:uuid]} #{loop_map}
+    tune2fs -U #{node.rightimage.node[:rightimage][:root_mount][:uuid]} #{node.rightimage.loop_map}
     
     mkdir $target_mnt
-    mount #{loop_map} $target_mnt
+    mount #{node.rightimage.loop_map} $target_mnt
 
     rsync -a $source_image/ $target_mnt/
 
